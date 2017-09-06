16d412e8da1ef88d1b5a6a0404eebd97858bb3ad
Jarrett <jserrian3@gatech.edu>
2017-09-06 16:18:25 -0400

    Tied together pages made by the team and added a few of my own

---
+++ app/src/main/java/com/example/christhai/fulcrum/EmotionalTrendsActivity.java
@@ -0,0 +1,13 @@
+package com.example.christhai.fulcrum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmotionalTrendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emotional_trends);
+    }
+}
---
+++ app/src/main/java/com/example/christhai/fulcrum/OverallWellnessActivity.java
@@ -0,0 +1,13 @@
+package com.example.christhai.fulcrum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OverallWellnessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overall_wellness);
+    }
+}
