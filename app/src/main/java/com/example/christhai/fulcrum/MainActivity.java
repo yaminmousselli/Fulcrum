9db6334687c244331f3464e858f6a24d5743c4f9
Jimmy Dinh-Nguyen <jpdn3@gatech.edu>
2017-09-08 20:25:25 -0400

    added icons for all the wellness
    - physical
    - emotional
    - social
    - academic
    
    changed theme colors
    changed login logo

--- app/src/main/java/com/example/christhai/fulcrum/MainActivity.java
+++ app/src/main/java/com/example/christhai/fulcrum/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.christhai.fulcrum;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +31,19 @@ public class MainActivity extends AppCompatActivity {
         AC = new AssessmentController();
         getParcel();
 
+        Button overallWellness = (Button) findViewById(R.id.overall_trends);
+        Button settings = (Button) findViewById(R.id.settings_temp);
+
+        ImageButton academicBar = (ImageButton) findViewById(R.id.academic_bar);
+        ImageButton emotionalBar = (ImageButton) findViewById(R.id.emotional_bar);
+        ImageButton physicalBar = (ImageButton) findViewById(R.id.physical_bar);
+        ImageButton socialBar = (ImageButton) findViewById(R.id.social_bar);
+
+        TextView academicText = (TextView) findViewById(R.id.academic_trend);
+        TextView emotionalText = (TextView) findViewById(R.id.emotional_trend);
+        TextView physicalText = (TextView) findViewById(R.id.physical_trend);
+        TextView socialText = (TextView) findViewById(R.id.social_trend);
+
         if (AC.checkComplete()) {
             mComplete.setVisibility(View.VISIBLE);
             mAssessment.setText("Complete!");
@@ -45,6 +61,86 @@ public class MainActivity extends AppCompatActivity {
                     intent.putExtra("questionNum", mQuestionNum);
                     startActivity(intent);
                 }
+            }
+        });
+
+        overallWellness.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), OverallWellnessActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        academicBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AcademicTrendsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        academicText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AcademicTrendsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        emotionalBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EmotionalTrendsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        emotionalText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EmotionalTrendsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        physicalBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PhysicalTrendsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        physicalText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PhysicalTrendsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        socialBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SocialTrendsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        socialText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SocialTrendsActivity.class);
+                startActivity(intent);
             }
         });
     }
