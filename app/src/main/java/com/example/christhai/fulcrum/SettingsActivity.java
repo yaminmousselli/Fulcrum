3b1a70cef7a270b0ab44c937f71898d6d21ed304
cthai3 <cthai3@gatech.edu>
2017-09-29 14:38:26 -0400

    Initial Hamburger menu commit

--- app/src/main/java/com/example/christhai/fulcrum/SettingsActivity.java
+++ app/src/main/java/com/example/christhai/fulcrum/SettingsActivity.java
@@ -1,8 +1,18 @@
 package com.example.christhai.fulcrum;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -15,7 +25,7 @@ import android.widget.TextView;
  * @version 1.0
 */
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,18 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("");
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.settings_drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         TextView account = (TextView) findViewById(R.id.account_settings);
         TextView notifications = (TextView) findViewById(R.id.notifications);
@@ -57,6 +79,53 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
         */
+    }
+
+    //Below are methods for the Toolbar.
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.nav_current_trends:
+                Menu m = item.getSubMenu();
+                if (!m.getItem(0).isVisible()) {
+                    for (int i = 0; i < 4; i++) {
+                        m.getItem(i).setVisible(true);
+                    }
+                } else {
+                    for (int i = 0; i < 4; i++) {
+                        m.getItem(i).setVisible(false);
+                    }
+                }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
 
